e44c67c8e64ad52c0426da876c9b624dc0d317a1
torik <riqibrekele@gmail.com>
2022-01-09 18:35:05 +0700

    add importing & contructing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
+import Head from 'next/head'
+
+// containers
+import { Blog, Features, Footer, GPT3, Header, Possibility } from '../containers'
+
+// components
+import { Navbar, CTA, Brand } from '../components'
+
 const Home = () => {
-  return (
-    <div>
-      this
+  return <>
+    <Head>
+      <title>MODERN WEB</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <GPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
+  </>
 }
 
 export default Home
