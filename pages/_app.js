52c25ea47fa3861e32dba439c00489c9e65d56a9
torik <riqibrekele@gmail.com>
2022-01-09 18:06:33 +0700

    add import styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,21 @@
+// styles
+// global
 import '../styles/globals.css'
+
+// article
+import '../styles/components/article/article.css'
+import '../styles/components/brand/brand.css'
+import '../styles/components/cta/cta.css'
+import '../styles/components/feature/feature.css'
+import '../styles/components/navbar/navbar.css'
+
+// container
+import '../styles/containers/blog/blog.css'
+import '../styles/containers/features/features.css'
+import '../styles/containers/footer/footer.css'
+import '../styles/containers/GPT3/gpt3.css'
+import '../styles/containers/header/header.css'
+import '../styles/containers/possibility/possibility.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
