3f7ff3da91b6c7dea92f56d48de670748f119317
torik <riqibrekele@gmail.com>
2022-01-09 18:31:20 +0700

    add index.js

---
+++ containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from '../containers/blog/Blog'
+export { default as Features } from '../containers/features/Features'
+export { default as Footer } from '../containers/footer/Footer'
+export { default as GPT3 } from '../containers/GPT3/GPT3'
+export { default as Header } from '../containers/header/Header'
+export { default as Possibility } from '../containers/possibility/Possibility'
