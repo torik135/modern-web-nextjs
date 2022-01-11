9b751587a726f684e231aff55e02b4d7482a289b
torik <riqibrekele@gmail.com>
2022-01-11 21:35:06 +0700

    add header component

--- containers/header/Header.js
+++ containers/header/Header.js
@@ -1,8 +1,44 @@
+// import Image from 'next/image'
+
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">This Webpage is MODERN Looking. And this text is in GRADIENT Color</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus iure, dolorum voluptatum doloremque provident architecto vitae pariatur sunt ipsum eligendi obcaecati, iusto unde cum id totam impedit, ullam libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus iure</p>
+
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your Email..." />
+                    <button type="button">Get Started</button>
+                </div>
+
+                <div className="gpt3__header-content__people">
+                    <div className="gpt3__header-content__people-container">
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                        <img src="/img/img3.jpg" alt="people" className="gpt3__header-content__people-item" />
+
+                    </div>
+                    <p>6000 people already join for the last 24 hours</p>
+                </div>
+
+            </div>
+            <div className="gpt3__header-image">
+                <img src="/img/img3.jpg" alt="header-image" />
+            </div>
+        </div >
     )
 }
 
