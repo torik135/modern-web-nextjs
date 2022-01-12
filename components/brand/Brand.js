f271e8756f6d4f3199499c77c6a7e980126728ad
torik <riqibrekele@gmail.com>
2022-01-12 18:38:11 +0700

    add brand component

--- components/brand/Brand.js
+++ components/brand/Brand.js
@@ -1,7 +1,24 @@
 const Brand = () => {
     return (
-        <div>
-            Brand
+        <div className="gpt3__brand">
+            <p>created using</p>
+            <div className="gpt3__brand-image section__padding">
+                <div>
+                    <img src='/svg/html.svg' alt="html" />
+                </div>
+                <div>
+                    <img src='/svg/css.svg' alt="css" />
+                </div>
+                <div>
+                    <img src='/svg/js.svg' alt="js" />
+                </div>
+                <div>
+                    <img src='/svg/git.svg' alt="git" />
+                </div>
+                <div>
+                    <img src='/svg/vscode.svg' alt="vscode" />
+                </div>
+            </div>
         </div>
     )
 }
