265967cca1c926d178003795c1754c54c0bb6148
torik <riqibrekele@gmail.com>
2022-01-14 18:41:41 +0700

    add gpt3 components

--- containers/GPT3/GPT3.js
+++ containers/GPT3/GPT3.js
@@ -1,7 +1,35 @@
+import Feature from '../../components/feature/Feature'
+
+const title1 = 'What is GPT-3'
+const text1 = 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+
+const title2 = 'Chatbots'
+const text2 = 'We so opinion friends me message as delight. Whole front do of plate heard oh ought.'
+
+const title3 = 'Knowledgebase'
+const text3 = 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+
+const title4 = 'Education'
+const text4 = 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+
+
 const GPT3 = () => {
     return (
-        <div>
-            GPT3
+        <div className="gpt3__whatgpt3 section__margin" id="gpt3">
+            <div className="gpt3__whatgpt3-feature">
+                <Feature title={title1} text={text1} />
+            </div>
+
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                <p>Explore the library</p>
+            </div>
+
+            <div className="gpt3__whatgpt3-container">
+                <Feature title={title2} text={text2} />
+                <Feature title={title3} text={text3} />
+                <Feature title={title4} text={text4} />
+            </div>
         </div>
     )
 }
