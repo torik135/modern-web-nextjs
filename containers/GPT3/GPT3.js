766bb601d6b4d4bb65d50e54b28edbae1f714cd1
torik <riqibrekele@gmail.com>
2022-01-16 18:03:08 +0700

    edit import

--- containers/GPT3/GPT3.js
+++ containers/GPT3/GPT3.js
@@ -1,4 +1,4 @@
-import Feature from '../../components/feature/Feature'
+import { Feature } from '../../components'
 
 const title1 = 'What is GPT-3'
 const text1 = 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
--- containers/features/Features.js
+++ containers/features/Features.js
@@ -1,4 +1,4 @@
-import Feature from '../../components/feature/Feature'
+import { Feature } from '../../components'
 
 const featuresData = [
     {
