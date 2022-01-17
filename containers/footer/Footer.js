1b64cc62e99a9cf9b86ea3d4d29a8fe385475eb1
torik <riqibrekele@gmail.com>
2022-01-17 22:03:07 +0700

    add footer components

--- containers/footer/Footer.js
+++ containers/footer/Footer.js
@@ -1,7 +1,41 @@
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="gpt3__footer section__padding">
+            <div className="gpt3__footer-heading">
+                <h1 className="gradient__text">Do you want to step into the <br />future before others</h1>
+            </div>
+            <div className="gpt3__footer-btn">
+                <p>Request Early Access</p>
+            </div>
+            <div className="gpt3__footer-links">
+                <div className="gpt3__footer-links_logo">
+                    <h3>Brand</h3>
+                    <p>123 Street Area<br />Brand, All Rights Reserved</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Links</h4>
+                    <p>Overons</p>
+                    <p>Social Media</p>
+                    <p>Counters</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Company</h4>
+                    <p>Terms & Conditions</p>
+                    <p>Privacy Policy</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Get in Touch</h4>
+                    <p>123 Street Area</p>
+                    <p>123-345-567</p>
+                    <p>email@email.com</p>
+                </div>
+            </div>
+
+            <div className="gpt3__footer-copy">
+                <p>&copy; 2022 Brand. All Rights Reserved</p>
+            </div>
         </div>
     )
 }
